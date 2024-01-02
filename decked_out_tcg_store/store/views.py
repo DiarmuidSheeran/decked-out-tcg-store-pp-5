17345fef36ac46bebca0417969094399445d66a3
Diarmuid Sheeran <dssheeran@outlook.com>
2024-01-02 22:11:25 +0000

    Add templates with views and urls to store app, Add placeholder content, set up base tamplate, footer and nav

--- decked_out_tcg_store/store/views.py
+++ decked_out_tcg_store/store/views.py
@@ -4,3 +4,23 @@ from django.shortcuts import render
 def landing(request):
     
     return render(request, 'landing.html')
+
+def about_us(request):
+    
+    return render(request, 'about_us.html')
+
+def contact_us(request):
+    
+    return render(request, 'contact_us.html')
+
+def cookies_policy(request):
+    
+    return render(request, 'cookies_policy.html')
+
+def news(request):
+    
+    return render(request, 'news.html')
+
+def returns_policy(request):
+    
+    return render(request, 'returns_policy.html')
