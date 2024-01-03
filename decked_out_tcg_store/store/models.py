50b09b53ed897e36ac4225762f62a1c897a77c33
Diarmuid Sheeran <dssheeran@outlook.com>
2024-01-03 22:19:34 +0000

    Create model for slideshow images

--- decked_out_tcg_store/store/models.py
+++ decked_out_tcg_store/store/models.py
@@ -1,3 +1,10 @@
 from django.db import models
 
 # Create your models here.
+class SlideshowImage(models.Model):
+    image = models.ImageField(upload_to='slideshow_images/')
+    link = models.URLField()
+    alt_text = models.CharField(max_length=255)
+
+    def __str__(self):
+        return self.alt_text
