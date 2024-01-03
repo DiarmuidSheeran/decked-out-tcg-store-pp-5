bd106288e4d6c5dee3a6dd32fd392b3c5c88cf52
Diarmuid Sheeran <dssheeran@outlook.com>
2024-01-03 14:05:06 +0000

    Create products and category models for products app

--- decked_out_tcg_store/product/admin.py
+++ decked_out_tcg_store/product/admin.py
@@ -1,3 +1,20 @@
 from django.contrib import admin
+from .models import Product, Category
 
-# Register your models here.
+class ProductAdmin(admin.ModelAdmin):
+    list_display = ['name', 'price', 'display_categories', 'quantity_available', 'is_featured', 'created_at', 'updated_at']
+    search_fields = ['name', 'categories__name']
+    list_filter = ['categories', 'is_featured']
+    prepopulated_fields = {'slug': ('name',)}
+
+    def display_categories(self, obj):
+        return ', '.join(category.name for category in obj.categories.all())
+
+    display_categories.short_description = 'Categories'
+
+class CategoryAdmin(admin.ModelAdmin):
+    list_display = ['name']
+    search_fields = ['name']
+
+admin.site.register(Product, ProductAdmin)
+admin.site.register(Category, CategoryAdmin)
--- decked_out_tcg_store/product/models.py
+++ decked_out_tcg_store/product/models.py
@@ -1,3 +1,29 @@
 from django.db import models
+from django.utils.text import slugify
 
-# Create your models here.
+class Category(models.Model):
+    name = models.CharField(max_length=255)
+
+    def __str__(self):
+        return self.name
+
+class Product(models.Model):
+    name = models.CharField(max_length=255)
+    description = models.TextField()
+    price = models.DecimalField(max_digits=10, decimal_places=2)
+    categories = models.ManyToManyField(Category)
+    image = models.ImageField(upload_to='product_images/', null=True, blank=True)
+    quantity_available = models.PositiveIntegerField(default=0)
+    is_featured = models.BooleanField(default=False)
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
+    slug = models.SlugField(unique=True, blank=True)
+    on_special_offer = models.BooleanField(default=False)
+    discounted_price = models.DecimalField(max_digits=10, decimal_places=2, null=True, blank=True)
+
+    def save(self, *args, **kwargs):
+        self.slug = slugify(self.name)
+        super().save(*args, **kwargs)
+
+    def __str__(self):
+        return self.name
