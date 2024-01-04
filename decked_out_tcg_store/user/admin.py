3f5eee1481ffc641180c02fa49ac64154eee2320
Diarmuid Sheeran <dssheeran@outlook.com>
2024-01-04 11:39:21 +0000

    Create UserProfile model

--- decked_out_tcg_store/user/admin.py
+++ decked_out_tcg_store/user/admin.py
@@ -1,3 +1,8 @@
 from django.contrib import admin
+from .models import UserProfile
 
-# Register your models here.
+@admin.register(UserProfile)
+class UserProfileAdmin(admin.ModelAdmin):
+    list_display = ('user', 'fname', 'lname', 'shipping_address_line_1', 'shipping_address_line_2', 'shipping_address_town', 'shipping_address_county', 'shipping_address_eircode')
+    search_fields = ('user__username', 'user__email', 'fname', 'lname')
+
--- decked_out_tcg_store/user/models.py
+++ decked_out_tcg_store/user/models.py
@@ -1,3 +1,18 @@
 from django.db import models
+from django.contrib.auth.models import User
+from django.templatetags.static import static
 
-# Create your models here.
+class UserProfile(models.Model):
+ 
+    user = models.OneToOneField(User, null=True, on_delete=models.CASCADE)
+    fname = models.CharField(max_length=200, null=True)
+    lname = models.CharField(max_length=200, null=True)
+    profile_pic = models.ImageField(upload_to='profile_pics/', null=True, blank=True, default=static('images/placeholder.jpg'))
+    shipping_address_line_1 = models.CharField(max_length=255, null=True, blank=True)
+    shipping_address_line_2 = models.CharField(max_length=255, null=True, blank=True)
+    shipping_address_town = models.CharField(max_length=255, null=True, blank=True)
+    shipping_address_county = models.CharField(max_length=20, null=True, blank=True)
+    shipping_address_eircode = models.CharField(max_length=255, null=True, blank=True)
+
+    def __str__(self):
+        return f"{self.fname} {self.lname}"
