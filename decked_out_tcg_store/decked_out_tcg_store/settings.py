0ddacf2cb6f1b1c65387a3b3e0c25b471eec9d7e
Diarmuid Sheeran <dssheeran@outlook.com>
2024-01-04 15:59:27 +0000

    Create cart.py and context_proccessor file and set upd session_id key for cart

--- decked_out_tcg_store/decked_out_tcg_store/settings.py
+++ decked_out_tcg_store/decked_out_tcg_store/settings.py
@@ -78,6 +78,7 @@ TEMPLATES = [
                 'django.template.context_processors.request',
                 'django.contrib.auth.context_processors.auth',
                 'django.contrib.messages.context_processors.messages',
+                'cart.context_processors.cart',
             ],
         },
     },
@@ -140,6 +141,8 @@ STATICFILES_DIRS = [
 MEDIA_URL = '/images/'
 
 MEDIA_ROOT = os.path.join(BASE_DIR, 'static/images')
+
+CART_SESSION_ID = 'cart'
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.2/ref/settings/#default-auto-field
