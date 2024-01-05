56929f577a5a4a7a41e19e9daca1d1f3586b97d6
Diarmuid Sheeran <dssheeran@outlook.com>
2024-01-05 09:38:07 +0000

    Add cart products to checkout view and add placeholder cart items to checkout template

--- decked_out_tcg_store/checkout/views.py
+++ decked_out_tcg_store/checkout/views.py
@@ -1,10 +1,19 @@
 from django.shortcuts import render
+from cart.cart import Cart
 
-# Create your views here.
 def checkout(request):
+    cart = Cart(request)
+    cart_products = cart.get_prods
+    quantities = cart.get_quants
+    totals = cart.cart_total()
 
-    return render(request, "checkout/checkout.html")
+    context = {
+        "cart_products":cart_products, 
+        "quantities":quantities, 
+        "totals":totals
+    }
+    return render(request, "checkout/checkout.html", context)
 
 def order_summary(request):
-
     return render(request, "checkout/order_summary.html")
+
