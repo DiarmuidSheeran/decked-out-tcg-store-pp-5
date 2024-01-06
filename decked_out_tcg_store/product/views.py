b40df840637b0477429567b80bbf52b0828ef10c
Diarmuid Sheeran <dssheeran@outlook.com>
2024-01-06 17:30:24 +0000

    Update checkbox to search multiple categories and and add filter by price feature, import q to views to allow complex logical or (|) querries to fillter both discounted and regular priced products

--- decked_out_tcg_store/product/views.py
+++ decked_out_tcg_store/product/views.py
@@ -2,20 +2,34 @@ from django.shortcuts import render, redirect, get_object_or_404
 from .models import Product, Category
 from .forms import EditProductForm, CreateProductForm, ProductSearchForm
 from cart.cart import Cart
+from django.db.models import Q
 
 # Create your views here.
 def product_list(request):
-    products = Product.objects.all()
     all_categories = Category.objects.all()
-    
     selected_category_ids = request.GET.getlist('categories[]')
-    if selected_category_ids:
-        selected_categories = Category.objects.filter(id__in=selected_category_ids)
-        products = products.filter(categories__in=selected_categories)
+    selected_categories = Category.objects.filter(id__in=selected_category_ids)
+    products = Product.objects.all()
+
+    for category in selected_categories:
+        products = products.filter(categories=category)
+
+    min_price = request.GET.get('min_price')
+    max_price = request.GET.get('max_price')
+
+    if min_price and max_price:
+        products = products.filter(
+            Q(price__range=(min_price, max_price)) | Q(on_special_offer=True, discounted_price__range=(min_price, max_price))
+        )
+        
+    special_offer = request.GET.get('special_offer')
+    if special_offer == 'true':
+        products = products.filter(on_special_offer=True)
 
     context = {
-        'products': products,
-        'categories':all_categories,
+        'products': products.distinct(),
+        'categories': all_categories,
+        'selected_category_ids': selected_category_ids,
     }
     return render(request, 'products/product_list.html', context)
 
