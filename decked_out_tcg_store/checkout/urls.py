6d7ef58af1bc5d28d92bec215b3f6b3c2c7845aa
Diarmuid Sheeran <dssheeran@outlook.com>
2024-01-06 09:30:06 +0000

    Remove stripe code from prooject

--- decked_out_tcg_store/checkout/urls.py
+++ decked_out_tcg_store/checkout/urls.py
@@ -4,5 +4,4 @@ from . import views
 urlpatterns = [
 	path('checkout/', views.checkout, name='checkout'),
     path('order_summary/', views.order_summary, name='order_summary'),
-    path('create_checkout_session/', views.create_checkout_session, name='create_checkout_session'),
 ]
--- decked_out_tcg_store/checkout/views.py
+++ decked_out_tcg_store/checkout/views.py
@@ -7,18 +7,6 @@ from product.models import Product
 from .forms import CheckoutForm
 from django.conf import settings
 from django.http import JsonResponse
-import stripe
-from django.views.decorators.csrf import csrf_exempt
-
-# views.py
-from django.shortcuts import render, redirect
-from cart.cart import Cart
-from user.models import UserProfile
-from django.contrib.auth.models import User
-from order.models import Order, OrderItem
-from product.models import Product
-from .forms import CheckoutForm
-from django.conf import settings
 
 def checkout(request):
     cart = Cart(request)
@@ -79,7 +67,6 @@ def checkout(request):
         "totals": totals,
         'shipping_address': shipping_address,
         'user_data': user_data,
-        'stripe_public_key': settings.STRIPE_PUBLIC_KEY
     }
     return render(request, "checkout/checkout.html", context)
 
@@ -92,30 +79,4 @@ def order_summary(request):
 
     context = {'latest_order': latest_order}
     return render(request, "checkout/order_summary.html", context)
-
-@csrf_exempt
-def create_checkout_session(request):
-    stripe.api_key = settings.STRIPE_SECRET_KEY
-    cart = Cart(request)
-    try:
-        session = stripe.checkout.Session.create(
-            payment_method_types=['card'],
-            line_items=[{
-                'price_data': {
-                    'currency': 'eur',
-                    'product_data': {
-                        'name': 'Your Product',
-                    },
-                    'unit_amount': int(cart.cart_total() * 100),  # Amount in cents
-                },
-                'quantity': 1,
-            }],
-            mode='payment',
-            success_url=request.build_absolute_uri('/success/'),
-            cancel_url=request.build_absolute_uri('/cancel/'),
-        )
-
-        return JsonResponse({'id': session.id})
-    except stripe.error.StripeError as e:
-        return JsonResponse({'error': str(e)}, status=402)
 
